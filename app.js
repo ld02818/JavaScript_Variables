d14dd08536bff421082e27dc2bf6b4648cb073cc
Luke Dixon <lukedixon41@yahoo.com>
2022-10-24 16:58:05 -0600

    updated

--- app.js
+++ app.js
@@ -3,8 +3,28 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
+const firstName = "Luke"
+let lastName = "Dixon";
+var age = 30;
 // Exercise 1
-
 // Exercise 2
+lastName = "Dixon"
+age = 30
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+let price = 19.99;
+let isOnSale = false;
+let salesPercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+let title = " Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
